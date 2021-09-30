bc859a2c00236c29d6e3548efb6b45bfdaa58530
Akarshika <88707878+akarshika23@users.noreply.github.com>
2021-09-30 19:49:52 +0530

    Add files via upload

---
+++ Assign-8.js
@@ -0,0 +1,15 @@
+function operators()
+        {
+            alert("hello eveyone");
+            document.write("hello,Welcome to my page:-");
+            var x=30;
+            var y=20;
+            z=x+y;
+            document.writeln(z);
+            b=x*y;
+            document.writeln(b);
+            c=x/y;
+            document.writeln(c);
+            a=x-y;
+            document.writeln(a);
+        }
---
+++ Assignment-7.js
@@ -0,0 +1,5 @@
+function Akarshika()
+        {       
+            alert("Hello Everyone ,My Name is Akarshika, Father's name= Alok Kumar Singh, Occupation:Teacher, Mother's name=Sneh Lata Singh,Occupation:Teacher");
+            document.write("Hello,Welcome to my page, and I m going to tell u about my sweet family Members where I LIVE LOVE AND LAUGH,[Firstly,i m going to tell about my loving and caring father and also the head of my family,A  person with kind and helful nature 'NAME-ALOK KUMAR SINGH'],[Secondly,One and only very close to me,my friend,my sister and mostly my sweet like honey,my Mother she is eveything to me[NAME-SNEH LATA SINGH],And At last my two younger brothers [Name-Anurag and Ashish singh].These are all my stars of my little world.");
+        }
